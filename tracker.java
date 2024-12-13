517cb5775a88e75e96d6d130fa23e868b48989cf
ZStout10 <68234015+ZStout10@users.noreply.github.com>
2024-12-13 15:26:09 -0500

    Update tracker.java
    Edits and debugging.

--- tracker.java
+++ tracker.java
@@ -8,7 +8,7 @@ import java.util.ArrayList;
 
 public class tracker {
 	// Variables and array lists
-	static int logIn;
+	static int logIn = -1;
 	static Scanner scan = new Scanner(System.in);
 	static ArrayList<UserProfile> profileList = new ArrayList<>();
 	
@@ -18,7 +18,7 @@ public class tracker {
 		String newName = scan.next();
 		System.out.println("Please enter your age: ");
 		int newAge = scan.nextInt();
-		System.out.println("Please enter your weight(lbs): ");
+		System.out.println("Please enter your weight(kgs): ");
 		double newWeight = scan.nextDouble();
 		
 		UserProfile newUser = new UserProfile(newName, newAge, newWeight);
@@ -28,61 +28,74 @@ public class tracker {
 	
 	// Adding a log in class to give the user a more personal experience
 	public static int logIn(ArrayList<UserProfile> list) {
-		int count = 1;
+		int count = 0;
 		System.out.println("Please select your profile: ");
 		for(UserProfile UserProfile: profileList ) {
 			System.out.println(count + ": " + UserProfile.getUserName());
+			count++;
 		}
 		logIn = scan.nextInt();
-		return logIn - 1;
+		while(logIn < 0 || logIn > profileList.size()-1) {
+			System.out.println("User not found. Please try again.");
+			logIn = scan.nextInt();
+		}
+		
+		return logIn;
 	}
 	
 	public static void main(String[] args) {
 		// Adding variables and profiles
 		boolean active = true;
 		int choice;
-		profileList.add(new UserProfile("Shawn", 23, 185.25));
-		profileList.add(new UserProfile("Angelica", 29, 131.75));
-		profileList.add(new UserProfile("Mark", 31, 202.00));
+		profileList.add(new UserProfile("Shawn", 23, 55.25));
+		profileList.add(new UserProfile("Angelica", 29, 42.00));
+		profileList.add(new UserProfile("Mark", 31, 102.00));
 		
 		// This while loop is the main program that asks a the user what they would like to do.
 		while(active == true) {
-			System.out.println("Hello, what would you like to do?");
-			System.out.println("1.Create a profile \n2.Select a profile \n0.Exit Program");
-			choice = scan.nextInt();
-			switch(choice) {
-				case 0:
-					active = false;
-					break;
-				case 1:
-					addNewProfile(profileList);
-					break;
-				case 2:
-					logIn(profileList);
-				default:
-					System.out.println("Incorrect input, please try again.");
-					break;
-			}	
-			
-			// This while loop is when a user is logged in
-			while (logIn != -1) {
-				System.out.println("Hello " + profileList.get(logIn).getUserName() + "!");
-				System.out.println("What would you like to do today?");
-				System.out.println("1.View exercises \n2.Select exercise \n3.Log out \n0.Exit program");
+			if(logIn == -1) {
+				choice = -1;
+				System.out.println("Hello, what would you like to do?");
+				System.out.println("1.Create a profile \n2.Select a profile \n0.Exit Program");
 				choice = scan.nextInt();
 				switch(choice) {
 					case 0:
 						active = false;
 						break;
 					case 1:
+						addNewProfile(profileList);
+						break;
+					case 2:
+						logIn(profileList);
+					default:
+						System.out.println("Incorrect input, please try again.");
+						break;
+				}
+			}	
+			// This while loop is when a user is logged in
+			while (logIn != -1) {
+				choice = -1;
+				System.out.println("Hello " + profileList.get(logIn).getUserName() + "!");
+				System.out.println("What would you like to do today?");
+				System.out.println("1.View exercises \n2.Select exercise \n3.Log out \n0.Exit program");
+				choice = scan.nextInt();
+				switch(choice) {
+					case 0:
+						logIn = -1;
+						active = false;
+						break;
+					case 1:
 						Exercise.displayPredefinedExercises();
+						break;
 					case 2:
 						Exercise.selectExercise();
+						break;
 					case 3:
 						logIn = -1;
 						break;
-				}
-				
+					default:
+						System.out.println("Invalid option. Please try again.");
+					}
 			}
 		}
 	}	
